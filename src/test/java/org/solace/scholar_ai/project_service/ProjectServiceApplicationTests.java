357095e0323bcf20a325d72bb5919135b38a71ee
Mst.Metaly Khatun <119936956+mstMetaly@users.noreply.github.com>
2025-10-29 00:02:08 +0600

    test and resources updated

---
+++ src/test/java/org/solace/scholar_ai/project_service/ProjectServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.solace.scholar_ai.project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
